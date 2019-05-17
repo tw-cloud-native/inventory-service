ca8740f94e581ae0d493063a40b01b30c0518c44
Luo Tao <lotreal@gmail.com>
2019-05-17 18:08:11 +0800

    ADD Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,20 @@
+node{
+    stage('Checkout') {
+        git url: 'https://github.com/tw-cloud-native/inventory-service.git',
+            branch: 'master'
+    }
+
+    stage('Test') {
+        sh 'ls -la'
+    }
+
+    stage('Build') {
+        sh './gradlew build'
+    }
+
+    stage('Deploy') {
+        withCredentials([file(credentialsId: 'KUBE_CONFIG', variable: 'KUBE_CONFIG')]) {
+            sh './deploy.sh'
+        }
+    }
+}
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,2 @@
 #!/usr/bin/env bash
-
-set -e
-
-echo "===Copy kube deployment file==="
-scp -o "StrictHostKeyChecking no" inventory-service-kube.yaml ubuntu@ec2-54-223-95-220.cn-north-1.compute.amazonaws.com.cn:/tmp/inventory-service-kube.yaml
-
-echo "===SSH into cluster==="
-ssh -o "StrictHostKeyChecking no" ubuntu@ec2-54-223-95-220.cn-north-1.compute.amazonaws.com.cn '
-set -e
-echo "===Deploy service and app==="
-kubectl apply -f /tmp/inventory-service-kube.yaml
-'
+kubectl --kubeconfig KUBE_CONFIG get nodes
