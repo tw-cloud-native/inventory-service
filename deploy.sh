a1fcac25e5f468fe65a9ef8450146617ebc03686
linwenjun <linwenjun@163.com>
2019-05-20 18:16:22 +0800

    fix string issue

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 TAG=$SERVICE-$BUILD_NUMBER
 IMAGE=$ECR_HOST/$TEAM:$TAG
-NAME_SPACE="$TEAM_ci"
+NAMESPACE="$TEAM_ci"
 
-sed "s#{{image}}#$IMAGE#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAME_SPACE#g" kube.yaml
+sed "s#{{image}}#$IMAGE#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAMESPACE#g" kube.yaml
 
-sed "s#{{image}}#$IMAGE#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAME_SPACE#g" kube.yaml | sudo kubectl --kubeconfig /tmp/kube-config apply -f -
+sed "s#{{image}}#$IMAGE#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAMESPACE#g" kube.yaml | sudo kubectl --kubeconfig /tmp/kube-config apply -f -
