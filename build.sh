3bddd27fadab168cd0d19a2d0eecf2f04abcf32b
linwenjun <linwenjun@163.com>
2019-05-20 15:21:22 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ curl http://tools.twu-china.cn/ | sudo sh -
 
 TAG=team-a-inventory-service
 
+./gradlew build
+
 sudo docker build -t $TAG .
 sudo docker tag $TAG:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/$TAG:latest
 sudo docker tag $TAG:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/$TAG:$BUILD_NUMBER
