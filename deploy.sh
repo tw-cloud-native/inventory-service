ea1c07c150550602548507c9192b8ab056655e29
linwenjun <linwenjun@163.com>
2019-05-20 15:39:48 +0800

    change namespace of inventory

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-kubectl --kubeconfig KUBE_CONFIG get nodes
+
+TAG=inventory-service-$BUILD_NUMBER
+IMAGE=076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/team-a:$TAG
+
+sed "s#{{image}}#$IMAGE#g" inventory-service-kube.yml | sudo kubectl --kubeconfig /tmp/kube-config apply -f -
