eb12cf80aee158ffd88998b2cb4a283b7e9a9aff
linwenjun <linwenjun@163.com>
2019-05-20 18:10:26 +0800

    test k8s config

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ TAG=$SERVICE-$BUILD_NUMBER
 IMAGE=$ECR_HOST/$TEAM:$TAG
 NAME_SPACE=$TEAM_ci
 
+sed "s#{{image}}#$IMAGE#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAME_SPACE#g" kube.yaml
+
 sed "s#{{image}}#$IMAGE#g; s#{{service}}#$SERVICE#g; s#{{namespace}}#$NAME_SPACE#g" kube.yaml | sudo kubectl --kubeconfig /tmp/kube-config apply -f -
