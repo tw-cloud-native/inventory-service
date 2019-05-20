de00d45cd93a5fd2e2debc891cd4ada55a5d60d1
linwenjun <linwenjun@163.com>
2019-05-20 15:40:58 +0800

    fix yml to yaml

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 TAG=inventory-service-$BUILD_NUMBER
 IMAGE=076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/team-a:$TAG
 
-sed "s#{{image}}#$IMAGE#g" inventory-service-kube.yml | sudo kubectl --kubeconfig /tmp/kube-config apply -f -
+sed "s#{{image}}#$IMAGE#g" inventory-service-kube.yaml | sudo kubectl --kubeconfig /tmp/kube-config apply -f -
