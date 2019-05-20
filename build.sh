8fd0339ac857592c41d6bcf4e4326bfb0ca32178
linwenjun <linwenjun@163.com>
2019-05-20 15:27:48 +0800

    fix tag issue

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@ set -ex
 
 curl http://tools.twu-china.cn/ | sudo sh -
 
-TAG=team-a-inventory-service
+TAG=inventory-service-$BUILD_NUMBER
 
 ./gradlew build
 
-sudo docker build -t $TAG .
-sudo docker tag $TAG:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/$TAG:latest
-sudo docker tag $TAG:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/$TAG:$BUILD_NUMBER
-sudo docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/$TAG:latest
-sudo docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/$TAG:$BUILD_NUMBER
+sudo docker build -t team-a:$TAG .
+sudo docker tag team-a:$TAG 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/team-a:$TAG
+sudo docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/team-a:$TAG
