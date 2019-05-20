8d4e65dc530913323cbb64e046635a5bc8967e72
linwenjun <linwenjun@163.com>
2019-05-20 15:59:57 +0800

    change db migration

--- src/main/resources/db/migration/V2018_11_28_16_00__create_table_inventory_and_inventory_lock.sql
+++ src/main/resources/db/migration/V2018_11_28_16_00__create_table_inventory_and_inventory_lock.sql
@@ -2,14 +2,12 @@ CREATE TABLE `inventory` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `product_id` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_PRODUCT_ID` (`product_id`)
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `inventory_lock` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `product_id` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `IDX_PRODUCT_ID` (`product_id`)
+  PRIMARY KEY (`id`)
 );
