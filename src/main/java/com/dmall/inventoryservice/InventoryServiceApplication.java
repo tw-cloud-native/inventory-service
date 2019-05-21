a4f27a1bfcd26523405961dd4573814863fc707e
linwenjun <linwenjun@163.com>
2019-05-21 09:48:03 +0800

    add eureka

--- src/main/java/com/dmall/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/dmall/inventoryservice/InventoryServiceApplication.java
@@ -2,9 +2,11 @@ package com.dmall.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableSwagger2
 public class InventoryServiceApplication {
 
